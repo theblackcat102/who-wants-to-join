b72efb4ab7b1da327b8d5b3bf04efcea2cb79494
theblackcat102 <zhirui09400@icloud.com>
2020-01-12 14:39:03 +0800

    test graph generator

--- random-modular-network-generator/random_modular_generator_variable_modules.py
+++ random-modular-network-generator/random_modular_generator_variable_modules.py
@@ -766,15 +766,16 @@ if __name__ == "__main__":
         print("Generating random modular graph with modularity(Q) = " +str(Q))
         print("The graph has " + str(N) + " nodes, " + str(m)+ " modules, and a network mean degree of " +str(d))
         print("Generating graph.....")
+        posfix = 'node_'+str(N) +'groups_' +str(m) + 'md_'+ str(d) + "_"
    
         G = generate_modular_networks(N, degfunction, modfunction, Q, m, d)
         
         print("Saving adjacency matrix under the filename adjacency_matrix.txt")
         adjacency_matrix = nx.to_numpy_matrix(G)
-        np.savetxt("adjacency_matrix.txt", adjacency_matrix, delimiter="\t")
+        np.savetxt(posfix+"adjacency_matrix.txt", adjacency_matrix, delimiter="\t")
         print("writing graph in the GraphML format under the filename random_modular_graph.graphml")
         # The graphml file can be uploaded in Gephi (http://gephi.github.io/) for graph visualization.
-        nx.write_graphml(G, "random_modular_graph.graphml")
+        nx.write_graphml(G, posfix+"random_modular_graph.graphml")
         
     except (IndexError, IOError):
         print("try again")
